Softmax backprop bug fix

CommonNodes.py
@@ -9,9 +9,10 @@ class InputNode(GraphNode):
 
     def __init__(self, value=None):
         super().__init__()
+        self._value = value
 
     def setValue(self, v):
-        self.value = v
+        self._value = v
 
     def forwardPass(self):
         pass
@@ -267,11 +268,11 @@ class ReduceMean(GraphNode):
             self.registerInEdges([p])
 
     def forwardPass(self):
-        self.value = np.sum(self.producer.value)/self.producer.value.shape[0]
+        self.value = np.sum(self.producer.value)/self.producer.value.size
 
     def backwardPass(self):
         self.producer.receiveGradient(self.totalGradient*
-            np.ones(self.producer.value.shape)/self.producer.value.shape[0])
+            np.ones(self.producer.value.shape)/self.producer.value.size)
 
 class LogSoftmax(GraphNode):
 
@@ -295,10 +296,8 @@ class LogSoftmax(GraphNode):
         self.value = (self.producer.value - self.cachedMaxVal) - np.log(np.sum(expValue, axis=1))[:, None]
 
     def backwardPass(self):
-        dy_dx = self.cache/np.sum(self.cache, axis=1)[:, None]
-        dy_dx = 1- dy_dx.shape[1]*dy_dx
-        #print(dy_dx, self.cache)
-        self.producer.receiveGradient(self.totalGradient*dy_dx)
+        tmp = self.cache/np.sum(self.cache, axis=1)[:, None]
+        self.producer.receiveGradient(self.totalGradient - tmp*np.sum(self.totalGradient, axis=1)[:, None])
 
 class NegativeLogLikelihoodLoss(GraphNode):
 
@@ -317,5 +316,5 @@ class NegativeLogLikelihoodLoss(GraphNode):
 
     def backwardPass(self):
         dy_dx = np.zeros(self.logits.value.shape)
-        dy_dx[self.booleanMask] = 1/self.classes.value.shape[0]
+        dy_dx[self.booleanMask] = -1/self.classes.value.shape[0]
         self.logits.receiveGradient(self.totalGradient*dy_dx)

testing.py
@@ -1,23 +1,34 @@
-import ExampleGraphs
+import ComputationalGraph
+import CommonNodes
 import numpy as np
-import matplotlib.pyplot as plt
+import torch 
 
-G = ExampleGraphs.InterestingGraph()
+G = ComputationalGraph.ComputationalGraph()
 
-xRange = np.arange(-1.5, 1.5, 0.001)
-yRange, d = G(xRange)
-pos = np.random.randint(0, xRange.shape[0], size=5)
+v = np.array([[1, 2, 3], [4, 5, 6]])
+i = CommonNodes.InputNode(v)
+c = CommonNodes.InputNode(np.array([1, 2]))
 
-xToDiff = xRange[pos]
-yVal = yRange[pos]
-slopes = d[pos]
+s = CommonNodes.LogSoftmax(i)
+l = CommonNodes.NegativeLogLikelihoodLoss(s, c)
+o = CommonNodes.OutputNode(l)
 
-plt.figure()
-plt.plot(xRange, yRange)
-for i in range(pos.shape[0]):
-    print(i)
-    plt.scatter(xToDiff[i], yVal[i], marker='x', color='red')
-    x = np.arange(xToDiff[i] - 0.2, xToDiff[i] + 0.2, 0.1)
-    y = x*slopes[i] + (yVal[i] - slopes[i]*xToDiff[i])
-    plt.plot(x, y, color='green')
-plt.show()
+G.addNode(i, "input")
+G.addNode(c, "classes")
+G.addNode(s, "logsoftmax")
+G.addNode(l, "nll")
+G.addNode(o, "out")
+
+G.runForwardPass()
+G.runBackwardPass()
+print(o.value, i.totalGradient)
+
+x = torch.tensor(v).float()
+cc = torch.tensor(np.array([1, 2]))
+x.requires_grad = True
+y = torch.nn.LogSoftmax(dim=1)
+z = torch.nn.NLLLoss()
+w = z(y(x), cc)
+w.backward()
+print(w, x.grad)
+#z = torch.mean(y, dim = )

testingClassification.py
@@ -16,9 +16,9 @@ allData = np.vstack((class1Data, class2Data))
 allClasses = np.concatenate((np.zeros(class1Data.shape[0]), np.ones(class2Data.shape[0])))
 
 whichModel = "mine"
-batchSize = 200
+batchSize = 100
 numEpochs = 500
-lr = 0.001
+lr = 0.1
 layerDims = [[2, 200], [200, 2]]
 
 if(whichModel == "mine"):